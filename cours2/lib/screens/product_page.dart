607fe6471853914df7d59495eabf4ad5a04018c6
Alexis Gauthron Marq <AlexisGauthronMarq@MacBook-Pro-4.local>
2026-01-31 18:23:57 +0100

    feat: update product_page

--- cours2/lib/screens/product_page.dart
+++ cours2/lib/screens/product_page.dart
@@ -4,6 +4,8 @@ import 'package:formation_flutter/model/product.dart';
 import 'package:formation_flutter/res/app_colors.dart';
 import 'package:formation_flutter/res/app_icons.dart';
 import 'package:formation_flutter/res/app_theme_extension.dart';
+import '../inherited/product_inherited.dart';
+
 
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
@@ -12,8 +14,12 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox.expand(
+
+    final Product product = generateProduct();
+
+    return ProductInherited(
+      product: product,
+      child: SizedBox.expand(
         child: Stack(
           children: [
             PositionedDirectional(
@@ -22,7 +28,7 @@ class ProductPage extends StatelessWidget {
               end: 0.0,
               height: IMAGE_HEIGHT,
               child: Image.network(
-                'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=1310&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                product.picture ?? '',
                 fit: BoxFit.cover,
                 cacheHeight:
                     (IMAGE_HEIGHT * MediaQuery.devicePixelRatioOf(context))
@@ -49,10 +55,13 @@ class ProductPage extends StatelessWidget {
                   crossAxisAlignment: .start,
                   children: [
                     Text(
-                      'Petits pois et carottes',
+                      product.name ?? '',
                       style: context.theme.title1,
                     ),
-                    Text('Cassegrain', style: context.theme.title2),
+                    Text(
+                      product.brands?.join(', ') ?? '',
+                      style: context.theme.title2,
+                    ),
                     Scores(),
                   ],
                 ),
@@ -70,6 +79,8 @@ class Scores extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final product = ProductInherited.of(context).product;
+
     return Column(
       children: [
         IntrinsicHeight(
@@ -78,18 +89,24 @@ class Scores extends StatelessWidget {
             children: [
               Expanded(
                 flex: 44,
-                child: _Nutriscore(nutriscore: ProductNutriScore.B),
+                child: _Nutriscore(
+                  nutriscore: product.nutriScore ?? ProductNutriScore.unknown,
+                ),
               ),
               VerticalDivider(),
               Expanded(
                 flex: 56,
-                child: _NovaGroup(novaScore: ProductNovaScore.group4),
+                child: _NovaGroup(
+                  novaScore: product.novaScore ?? ProductNovaScore.unknown,
+                ),
               ),
             ],
           ),
         ),
         Divider(),
-        _GreenScore(greenScore: ProductGreenScore.A),
+        _GreenScore(
+          greenScore: product.greenScore ?? ProductGreenScore.unknown,
+        ),
       ],
     );
   }
